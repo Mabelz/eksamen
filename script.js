6ca38880fb42b76ed4ba4e56bd5793206b954521
Zeinab <zizi-33@hotmail.com>
2018-05-29 19:09:55 +0200

    hejsa

--- script.js
+++ script.js
@@ -1,33 +1,89 @@
- //navigationsbar: Skift mellem at tilføje og fjerne den responsive klasse til topnav, når brugeren klikker på ikonet-->
- function myFunction() {
-     var x = document.getElementById("myTopnav");
-     if (x.className === "topnav") {
-         x.className += " responsive";
-     } else {
-         x.className = "topnav";
-     }
- }
+// Get the modal
+var modal = document.getElementById("myModal");
 
-//SLIDESHOW 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    var slideIndex = 0;
-            showSlides();
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-            function showSlides() {
-                var i;
-                var slides = document.getElementsByClassName("mySlides");
-                var dots = document.getElementsByClassName("dot");
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";
-                }
-                slideIndex++;
-                if (slideIndex > slides.length) {
-                    slideIndex = 1
-                }
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex - 1].style.display = "block";
-                dots[slideIndex - 1].className += " active";
-                setTimeout(showSlides, 4000); // Change image every 2 seconds
-            }
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+//KNAP2
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+}
+
+
+//KNAP3
+
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close3")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal3.style.display = "none";
+    }
+}
+
+
+///kalender
+$(function () {
+    $("#datepicker").datepicker();
+});
